818a98880bc3f63c14b24ae98145293b9972e7e8
ssengerb <sanzhar_1710@mail.ru>
2023-09-28 22:23:04 +0600

    handlers added

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/smslash/rest_api_app/pkg/handler"
 	todo "github.com/smslash/rest_api_app/tmp"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running HTTP server: %s", err.Error())
 	}
 }
